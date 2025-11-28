faf22d12447551258cb4a1bbe57ca105b3648f63
walbarellos <willianalbarello@gmail.com>
2025-11-28 03:22:54 -0500

    update: src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import React, { useState, useCallback, useMemo } from 'react';
+
 import QRCode from "react-qr-code";
 import { 
   User, Github, ExternalLink, Sparkles, TrendingUp, Award, 
